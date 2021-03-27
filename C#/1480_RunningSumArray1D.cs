3b9299846213d117ead29792adb01d27824daf94
unknown <manuel.rubio.arias@gmail.com>
2021-03-27 11:09:58 +0100

    First Commit

---
+++ C#/1480_RunningSumArray1D.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace RunningSum1D
+{
+    public class Solution
+    {
+        public static int[] RunningSum(int[] nums)
+        {
+            int i = 0;
+            int j = 0;
+            int[] result = { 0, 0, 0, 0 };
+            foreach (int nb in nums)
+            {
+                result[i] = nb;
+                j = i;
+                while (j >= 0)
+                {
+                    if (j != i)
+                        result[i] += nums[j];
+                    j--;
+                }
+                i++;
+            }
+            return (result);
+        }
+    }
+}
