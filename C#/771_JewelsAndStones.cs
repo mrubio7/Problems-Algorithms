d0dce70e55bc551897a6e8fd0a989b52f08c36ef
unknown <manuel.rubio.arias@gmail.com>
2021-03-29 10:29:44 +0200

    771

---
+++ C#/771_JewelsAndStones.cs
@@ -0,0 +1,18 @@
+﻿public class Solution
+{
+	public static int NumJewelsInStones(string jewels, string stones)
+	{
+		char[] arrJewels = jewels.ToCharArray();
+		char[] arrStones = stones.ToCharArray();
+		int res = 0;
+		foreach (char j in arrJewels)
+		{
+			foreach (char s in arrStones)
+			{
+				if (j == s)
+					res++;
+			}
+		}
+		return (res);
+	}
+}
